87a9698018e498f35d405c3ffcab52d32b590a88
Jeff DeWitte <JD@jeffreys-mbp.attlocal.net>
2018-11-22 16:54:40 -0500

    Basic sample setup for loading xibs.

---
+++ SaltCount/Data/DataStore.swift
@@ -0,0 +1,13 @@
+//
+//  DataStore.swift
+//  SaltCount
+//
+//  Created by Jeff DeWitte on 11/22/18.
+//  Copyright © 2018 Jeff DeWitte. All rights reserved.
+//
+
+import UIKit
+
+class DataStore: NSObject {
+    
+}
---
+++ SaltCount/VCs/MainController.swift
@@ -0,0 +1,19 @@
+//
+//  Main Controller.swift
+//  SaltCount
+//
+//  Created by Jeff DeWitte on 11/22/18.
+//  Copyright © 2018 Jeff DeWitte. All rights reserved.
+//
+
+import UIKit
+
+class MainController: NSObject {
+
+    private let dataStore: DataStore
+    
+    init(dataStore: DataStore) {
+        self.dataStore = dataStore
+    }
+    
+}
